5ad0aeb19bc284b7493974633e2e9e0122cf1e30
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-19 15:41:49 +0530

    Create mongo.py

---
+++ Kaamam/utils/mongo.py
@@ -0,0 +1,16 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_DB_URI")
+mongo_client = pymongo.MongoClient(MONGO_URI)
+db = mongo_client["vc_music"]
+log_col = db["logs"]
+
+def log_song(chat_id, user_id, title):
+    log_col.insert_one({
+        "chat_id": chat_id,
+        "user_id": user_id,
+        "title": title
+    })
