aa6b08345bc847fc3e42230e84bc37340f94202c
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-19 17:08:05 +0530

    Update mongo.py

--- Kaamam/utils/mongo.py
+++ Kaamam/utils/mongo.py
@@ -1,12 +1,16 @@
 import pymongo
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_DB_URI")
 mongo_client = pymongo.MongoClient(MONGO_URI)
 db = mongo_client["vc_music"]
 log_col = db["logs"]
+
+def connect_mongo():
+    return db
 
 def log_song(chat_id, user_id, title):
     log_col.insert_one({
