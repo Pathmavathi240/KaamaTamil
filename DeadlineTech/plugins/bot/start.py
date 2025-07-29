1f59ab8743282cf588accefac0b3b4c134516fd6
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-29 20:41:39 +0530

    Update start.py

--- DeadlineTech/plugins/bot/start.py
+++ DeadlineTech/plugins/bot/start.py
@@ -145,7 +145,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        photo=config.STATS_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
