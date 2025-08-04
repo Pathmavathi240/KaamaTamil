245c47689ac6500683836527e73d215a3f0d5a32
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-08-04 09:27:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_KEY = getenv("API_KEY")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 720))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
